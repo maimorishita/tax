aa5329ddf08f9059116aabfe66027e9132a7ad34
morishita <morishita@isken.co.jp>
2013-07-26 15:03:40 +0900

    初期データの追加など

--- src/main/java/jp/co/isken/tax/run/Initializer.java
+++ src/main/java/jp/co/isken/tax/run/Initializer.java
@@ -21,6 +21,27 @@ import jp.co.isken.tax.util.Util;
 public class Initializer {
 
 	public static void init() {
+		datainit();
+		
+		Term term = new Term(Util.stringToDate("20120401000000"),
+				Util.stringToDate("21000401000000"));
+
+		Tax tax = new Tax(HardCode.EXCISE);
+		BigDecimal bd = new BigDecimal("0.05");
+		
+		Item ei1 = new Item("ナックバーガー");
+		Item ei2 = new Item("ナックチーズバーガー");
+		Item ei3 = new Item("ナックポテトS");
+		Item ei4 = new Item("ナックポテトM");
+		Item ei5 = new Item("ナックコーラ");
+		Item ei6 = new Item("ナックシェーク");
+		TaxRate rate1 = new TaxRate(tax, bd, ei1, term);
+		TaxRate rate2 = new TaxRate(tax, bd, ei2, term);
+		TaxRate rate3 = new TaxRate(tax, bd, ei3, term);
+		TaxRate rate4 = new TaxRate(tax, bd, ei4, term);
+		TaxRate rate5 = new TaxRate(tax, bd, ei5, term);
+		TaxRate rate6 = new TaxRate(tax, bd, ei6, term);
+
 		Product nacburger = new Product("ナックバーガー", 90);
 		Product naccheeseburger = new Product("ナックチーズバーガー", 120);
 		Product nacpotato_s = new Product("ナックポテトS", 150);
@@ -28,30 +49,16 @@ public class Initializer {
 		Product naccola = new Product("ナックコーラ", 150);
 		Product nacshake = new Product("ナックシェーク", 200);
 
-		// Category food = new Category("フード");
-		// Category drink = new Category("ドリンク");
-
-		// food.add(nacburger);
-		// food.add(naccheeseburger);
-		// food.add(nacpotato_s);
-		// food.add(nacpotato_m);
-		// drink.add(naccola);
-		// drink.add(nacshake);
-
 		Party p1 = new Party("SATO");
 		Party p2 = new Party("GOTO");
 		Party p3 = new Party("MUTO");
+		
+		Contract c = new Contract(Util.stringToDate("20130707000000"), p1,
+				"切捨て", "計上日");
 	}
 
 	public static void test() {
-		Contract.init();
-		Product.init();
-		Transaction.init();
-		CFTransaction.init();
-		Entry.init();
-		CFEntry.init();
-		Account.init();
-		CFAccount.init();
+		datainit();
 
 		Item ei1 = new Item("ナックバーガー");
 		Item ei2 = new Item("ナックチーズバーガー");
@@ -77,6 +84,17 @@ public class Initializer {
 		Contract c = new Contract(Util.stringToDate("20130707000000"), p1,
 				"切捨て", "計上日");
 
+	}
+
+	public static void datainit() {
+		Contract.init();
+		Product.init();
+		Transaction.init();
+		CFTransaction.init();
+		Entry.init();
+		CFEntry.init();
+		Account.init();
+		CFAccount.init();
 	}
 
 }
